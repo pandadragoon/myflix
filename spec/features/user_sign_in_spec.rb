d17fd743ffc266720e35a2c8ecbe2e8595d9f366
Joshua Edwards <pandadragoon@gmail.com>
2014-10-21 12:05:08 -0500

    added user interacts with the video queue feature spec test

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 feature 'user signs in' do
   scenario "with valid email and password" do
     bob = Fabricate(:user)
-    visit sign_in_path
-    fill_in "Email Address", with: bob.email
-    fill_in "Password", with: bob.password
-    click_button "Sign In"
+    sign_in(bob)
     expect(page).to have_content bob.full_name
   end
 end
