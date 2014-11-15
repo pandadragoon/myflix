86296b41610df0778c94f7678e9d098880d0d525
Josh Edwards <josh.dean.edwards@gmail.com>
2014-11-14 17:09:11 -0800

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,17 @@ Category.create(name: 'TV Comedies')
 Category.create(name: 'Comedies')
 Category.create(name: 'Animation')
 
-Video.create(title: 'Family Guy', description: 'A guy and his family.', small_cover_url: 'tmp/family_guy.jpg', large_cover_url: 'tmp/monk_large.jpg', category_id: 1)
-Video.create(title: 'Futurama', description: 'A guy in the future.', small_cover_url: 'tmp/futurama.jpg', large_cover_url: 'tmp/monk_large.jpg', category_id: 1)
-monk = Video.create(title: 'Monk', description: 'A guy named Monk.', small_cover_url: 'tmp/monk.jpg', large_cover_url: 'tmp/monk_large.jpg', category_id: 1)
-Video.create(title: 'South Park', description: 'Some kids in South Park', small_cover_url: 'tmp/south_park.jpg', large_cover_url: 'tmp/monk_large.jpg', category_id: 1)
+Video.create(title: 'Family Guy', description: 'A guy and his family.', small_cover: 'tmp/family_guy.jpg', large_cover: 'tmp/monk_large.jpg', category_id: 1)
+Video.create(title: 'Futurama', description: 'A guy in the future.', small_cover: 'tmp/futurama.jpg', large_cover: 'tmp/monk_large.jpg', category_id: 1)
+monk = Video.create(title: 'Monk', description: 'A guy named Monk.', small_cover: 'tmp/monk.jpg', large_cover: 'tmp/monk_large.jpg', category_id: 1)
+Video.create(title: 'South Park', description: 'Some kids in South Park', small_cover: 'tmp/south_park.jpg', large_cover: 'tmp/monk_large.jpg', category_id: 1)
 
-Video.create(title: 'Futurama', description: 'A guy in the future.', small_cover_url: 'tmp/futurama.jpg', large_cover_url: 'tmp/monk_large.jpg', category_id: 2)
-Video.create(title: 'Monk', description: 'A guy named Monk.', small_cover_url: 'tmp/monk.jpg', large_cover_url: 'tmp/monk_large.jpg', category_id: 2)
-Video.create(title: 'South Park', description: 'Some kids in South Park', small_cover_url: 'tmp/south_park.jpg', large_cover_url: 'tmp/monk_large.jpg', category_id: 2)
+Video.create(title: 'Futurama', description: 'A guy in the future.', small_cover_: 'tmp/futurama.jpg', large_cover: 'tmp/monk_large.jpg', category_id: 2)
+Video.create(title: 'Monk', description: 'A guy named Monk.', small_cover: 'tmp/monk.jpg', large_cover: 'tmp/monk_large.jpg', category_id: 2)
+Video.create(title: 'South Park', description: 'Some kids in South Park', small_cover_: 'tmp/south_park.jpg', large_cover: 'tmp/monk_large.jpg', category_id: 2)
 
-Video.create(title: 'Monk', description: 'A guy named Monk.', small_cover_url: 'tmp/monk.jpg', large_cover_url: 'tmp/monk_large.jpg', category_id: 3)
-Video.create(title: 'South Park', description: 'Some kids in South Park', small_cover_url: 'tmp/south_park.jpg', large_cover_url: 'tmp/monk_large.jpg', category_id: 3)
+Video.create(title: 'Monk', description: 'A guy named Monk.', small_cover: 'tmp/monk.jpg', large_cover_: 'tmp/monk_large.jpg', category_id: 3)
+Video.create(title: 'South Park', description: 'Some kids in South Park', small_cover: 'tmp/south_park.jpg', large_cover: 'tmp/monk_large.jpg', category_id: 3)
 
 panda = User.create(email: 'panda@panda.com', full_name: 'Panda Edwards', password: 'password')
 
