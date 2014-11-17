925a3abc831ffb965059e6303fc3aa4aa12fa62b
Joshua Edwards <josh.dean.edwards@gmail.com>
2014-11-17 15:38:04 -0800

    created signup service and tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,108 +7,43 @@ describe UsersController do
       expect(assigns(:user)).to be_instance_of(User)
     end
   end
+
   describe "POST create" do
 
-    context "with valid personal info and valid card" do
-      let(:charge){double(:charge, successful?: true)}
-      before do
-        StripeWrapper::Charge.should_receive(:create).and_return(charge)
-      end
+    context "successful user sign up" do
+
       after {ActionMailer::Base.deliveries.clear}
-      it "creates the user" do
-        post :create, user: Fabricate.attributes_for(:user)
-        expect(User.count).to eq(1)
-      end
+
       it "redirects to the sign in page" do
+        result = double(:sign_up_result, successful?: true)
+        UserSignup.any_instance.should_receive(:sign_up).and_return(result)
         post :create, user: Fabricate.attributes_for(:user)
         expect(response).to redirect_to sign_in_path
       end
-      it "makes the user follow the inviter" do
-        alice = Fabricate(:user)
-        invitation = Fabricate(:invitation, inviter: alice, recipient_email: 'joe@example.com')
-        post :create, user: {email: 'joe@example.com', password: "password", full_name: 'Joe Doe'}, invitation_token: invitation.token
-        joe = User.where(email: 'joe@example.com').first
-        expect(joe.follows?(alice)).to be_true
-      end
-      it "makes the inviter follow the user" do
-        alice = Fabricate(:user)
-        invitation = Fabricate(:invitation, inviter: alice, recipient_email: 'joe@example.com')
-        post :create, user: {email: 'joe@example.com', password: "password", full_name: 'Joe Doe'}, invitation_token: invitation.token
-        joe = User.where(email: 'joe@example.com').first
-        expect(alice.follows?(joe)).to be_true
-      end
-      it "expires the invitation upon acceptance" do
-        alice = Fabricate(:user)
-        invitation = Fabricate(:invitation, inviter: alice, recipient_email: 'joe@example.com')
-        post :create, user: {email: 'joe@example.com', password: "password", full_name: 'Joe Doe'}, invitation_token: invitation.token
-        expect(Invitation.first.token).to be_nil
-      end
     end
 
-    context "valid personal info and declined card" do
+    context "failed user sign up" do
       it "renders the new template" do
-        charge = double(:charge, successful?: false, error_message: "Your card was declined.")
-        StripeWrapper::Charge.should_receive(:create).and_return(charge )
+        result = double(:sign_up_result, successful?: false, error_message: "This is an error message")
+        UserSignup.any_instance.should_receive(:sign_up).and_return(result)
         post :create, user: Fabricate.attributes_for(:user), stripeToken: '1231241'
         expect(response).to render_template :new
       end
-      it "does not create a new user record" do
-        charge = double(:charge, successful?: false, error_message: "Your card was declined.")
-        StripeWrapper::Charge.should_receive(:create).and_return(charge )
-        post :create, user: Fabricate.attributes_for(:user), stripeToken: '1231241'
-        expect(User.count).to eq(0)
-      end
+
       it "sets the flash error message" do
-        charge = double(:charge, successful?: false, error_message: "Your card was declined.")
-        StripeWrapper::Charge.should_receive(:create).and_return(charge )
+        result = double(:sign_up_result, successful?: false, error_message: "This is an error message")
+        UserSignup.any_instance.should_receive(:sign_up).and_return(result)
         post :create, user: Fabricate.attributes_for(:user), stripeToken: '1231241'
         expect(flash[:error]).to be_present
       end
     end
-
-    context "with invalid personal info" do
-      before do
-        post :create, user: {password: "password", full_name: "Josh Edwards"}
-      end
-      it "does not create a user" do
-        expect(User.count).to eq(0)
-      end
-      it "renders the :new template" do
-        expect(response).to render_template :new
-      end
-      it "sets @user" do
-        expect(assigns(:user)).to be_instance_of(User)
-      end
-      it "does not charge the card" do
-        StripeWrapper::Charge.should_not_receive(:create)
-      end
-      it "does not send out email with invalid inputs" do
-        post :create, user: {email: "joe@example.com"}
-        expect(ActionMailer::Base.deliveries).to be_empty
-      end
-    end
-    context "sending emails" do
-      let(:charge) {double(:charge, successful?: true)}
-      before do
-        StripeWrapper::Charge.should_receive(:create).and_return(charge)
-      end
-      after {ActionMailer::Base.deliveries.clear}
-      it "sends out email to the user with valid inputs" do
-        post :create, user: {email: "joe@example.com", password: "password", full_name: "Joe Smith"}
-        expect(ActionMailer::Base.deliveries.last.to).to eq(["joe@example.com"])
-      end
-      it "sends out email containing the user's name with valid inputs" do
-        post :create, user: {email: "joe@example.com", password: "password", full_name: "Joe Smith"}
-        expect(ActionMailer::Base.deliveries.last.body).to include("Joe Smith")
-      end
-    end
-
-
   end
+
   describe 'GET show' do
     it_behaves_like "requires sign in" do
       let(:action) {get :show, id: 3}
     end
+
     it "sets @user" do
       set_current_user
       alice = Fabricate(:user)
@@ -124,16 +59,19 @@ describe UsersController do
       get :new_with_invitation_token, token: invitation.token
       expect(response).to render_template :new
     end
+
     it "sets @user with recipient's email" do
       invitation = Fabricate(:invitation)
       get :new_with_invitation_token, token: invitation.token
       expect(assigns(:user).email).to eq(invitation.recipient_email)
     end
+
     it "sets @invitation_token" do
       invitation = Fabricate(:invitation)
       get :new_with_invitation_token, token: invitation.token
       expect(assigns(:invitation_token)).to eq(invitation.token)
     end
+
     it "redirects to expired token page for invalid tokens" do
       get :new_with_invitation_token, token: 'adslfkjs'
       expect(response).to redirect_to expired_token_path
